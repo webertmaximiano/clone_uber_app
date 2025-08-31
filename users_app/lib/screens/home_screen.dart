125972a6385b9ad8aa999ab6fe7d6423362c167a
Webert Maximiano <webertcoach@gmail.com>
2025-08-31 13:15:12 -0300

    Removido conflitos de chaves, sistema logando via web

--- users_app/lib/screens/home_screen.dart
+++ users_app/lib/screens/home_screen.dart
@@ -1,41 +1,48 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart'; // Importação para Firebase Auth
-import 'package:users_app/screens/authentication/login_screen.dart'; // Importação para a tela de Login
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart'; // Import para o Google Maps
+import 'package:users_app/screens/authentication/login_screen.dart';
 
-/// Tela Principal do Aplicativo
-///
-/// Esta será a tela que o usuário verá após fazer login com sucesso.
-/// Por enquanto, é um placeholder simples.
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  // Posição inicial da câmera do mapa.
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Bem-vindo!'),
+        title: const Text('Uber Clone'),
         centerTitle: true,
         actions: [
           // Botão de Logout
           IconButton(
-            icon: const Icon(Icons.logout), // Ícone de logout
+            icon: const Icon(Icons.logout),
             onPressed: () async {
-              // Lógica para fazer logout do Firebase.
               await FirebaseAuth.instance.signOut();
-              // Após o logout, navega para a tela de Login e remove todas as rotas anteriores.
               Navigator.of(context).pushAndRemoveUntil(
                 MaterialPageRoute(builder: (context) => const LoginScreen()),
-                (Route<dynamic> route) => false, // Remove todas as rotas anteriores
+                (Route<dynamic> route) => false,
               );
             },
           ),
         ],
       ),
-      body: const Center(
-        child: Text(
-          'Você está logado!',
-          style: TextStyle(fontSize: 24),
-        ),
+      body: GoogleMap(
+        initialCameraPosition: _kGooglePlex,
+        mapType: MapType.normal, // Tipo de mapa: normal, satélite, terreno, híbrido
+        myLocationEnabled: true, // Mostra o botão 'minha localização'
+        zoomControlsEnabled: true, // Mostra os controles de zoom
       ),
     );
   }
